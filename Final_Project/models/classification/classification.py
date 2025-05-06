f9b22c516b3ef98a0598ddc50aecde98bfc26b71
Bhavana-bhuvana <bhuvanashivakumar1404@gmail.com>
2025-05-06 20:57:49 +0530

    Update classification.py

--- Final_Project/models/classification/classification.py
+++ Final_Project/models/classification/classification.py
@@ -7,14 +7,6 @@ from models.classification.knn_classification import knn_classification_page
 from models.classification.naive_bayes_classification import naive_bayes_classification_page
 
 def classification_page(model_name, data):
-    st.subheader("Select Features and Target")
-    features = st.multiselect("Select feature columns (X):", options=data.columns)
-    target = st.selectbox("Select target column (y):", options=data.columns)
-
-    if not features or not target or target in features:
-        st.warning("Please select valid feature(s) and target.")
-        return
-
     if model_name == "Logistic Regression":
         logistic_regression_page(data)
     elif model_name == "Decision Tree":
