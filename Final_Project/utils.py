cda903ef5c74a1d94a0aba5207f815491798957b
Bhavana-bhuvana <bhuvanashivakumar1404@gmail.com>
2025-05-12 08:42:49 +0530

    Update utils.py

--- Final_Project/utils.py
+++ Final_Project/utils.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-
+import smtplib
+from email.message import EmailMessage
 def load_data(uploaded_file):
     try:
         if uploaded_file.name.endswith('.csv'):
@@ -22,3 +23,16 @@ def reset_session():
     st.session_state.uploaded_data = None
     st.session_state.active_models = []
     st.session_state.model_results = {}
+
+
+
+def send_otp_email(receiver_email, otp):
+    msg = EmailMessage()
+    msg.set_content(f"Your OTP for registration is: {otp}")
+    msg['Subject'] = "Your OTP Code"
+    msg['From'] = "komal0mallaram@gmail.com"
+    msg['To'] = receiver_email
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login("komal0mallaram@gmail.com", "wvma uvep ocew kodq")  # Use env vars in real apps
+        smtp.send_message(msg)
